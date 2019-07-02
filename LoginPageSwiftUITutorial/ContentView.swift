eb7e7dd2d60513a4733cb809b3f11f500d77a1da
BLCKBIRDS <andreasschultz@Andreass-MacBook-Pro.local>
2019-07-01 22:48:55 -0700

    Project added

---
+++ LoginPageSwiftUITutorial/ContentView.swift
@@ -0,0 +1,80 @@
+//
+//  ContentView.swift
+//  LoginPageSwiftUITutorial
+//
+//  Created by Andreas Schultz on 01.07.19.
+//  Copyright © 2019 Andreas Schultz. All rights reserved.
+//
+
+import SwiftUI
+
+let lightGreyColor = Color(red: 239.0/255.0, green: 243.0/255.0, blue: 244.0/255.0, opacity: 1.0)
+
+struct ContentView : View {
+    
+    @State var username: String = ""
+    @State var password: String = ""
+    
+    var body: some View {
+        
+        VStack {
+            WelcomeText()
+            UserImage()
+            TextField($username, placeholder: Text("Username"))
+                .padding()
+                .background(lightGreyColor)
+                .cornerRadius(5.0)
+                .padding(.bottom, 20)
+            SecureField($password, placeholder: Text("Password"))
+                .padding()
+                .background(lightGreyColor)
+                .cornerRadius(5.0)
+                .padding(.bottom, 20)
+            Button(action: {print("Button tapped")}) {
+               LoginButtonContent()
+            }
+        }
+        .padding()
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
+
+struct WelcomeText : View {
+    var body: some View {
+        return Text("Welcome!")
+            .font(.largeTitle)
+            .fontWeight(.semibold)
+            .padding(.bottom, 20)
+    }
+}
+
+struct UserImage : View {
+    var body: some View {
+        return Image("userImage")
+            .resizable()
+            .aspectRatio(UIImage(named: "userImage")!.size, contentMode: .fill)
+            .frame(width: 150, height: 150)
+            .clipped()
+            .cornerRadius(150)
+            .padding(.bottom, 75)
+    }
+}
+
+struct LoginButtonContent : View {
+    var body: some View {
+        return Text("LOGIN")
+            .font(.headline)
+            .color(.white)
+            .padding()
+            .frame(width: 220, height: 60)
+            .background(Color.green)
+            .cornerRadius(15.0)
+    }
+}
